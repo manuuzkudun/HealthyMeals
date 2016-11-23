5f38e7cd8f10fd00f7f980303017fb62bd7fd3aa
Manu Uzkudun <uzkudun@mdpi.com>
2016-11-23 16:11:55 +0100

    Features section styled

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -6,7 +6,7 @@ import style from "./features.less";
 const Features = () => (
   <section className="features">
     <div className="features__container">
-      <h2 className="features__headline">Get food fast — not fast food</h2>
+      <h2 className="features__headline">Get food fast &mdash; not fast food</h2>
       <p className="features__p">Hello, we're Omnifood, your new premium food delivery service. We know you're always busy. No time for cooking. So let us take care of that, we're really good at it, we promise!</p>
       <div className="features__list">
         { featuresData.map( (feature,index) => <Feature key={index} feature={feature} />) }
